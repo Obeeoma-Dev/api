4eb151a0c55445a875899570f05b9b1ba53c10d2
kulabako josephine <josephinekentrhines@gmail.com>
2025-09-24 16:39:45 +0300

    auth

--- obeeomaapp/models.py
+++ obeeomaapp/models.py
@@ -4,7 +4,13 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('systemadmin', 'System Admin'),
+        ('employer', 'Employer'),
+        ('employee', 'Employee'),
+    )
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='employee')
     onboarding_completed = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.username
+        return f"{self.username} ({self.role})"
