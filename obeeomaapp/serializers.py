22752896e02e347662c3bd5da7ab8e80bb1d3fe9
kulabako josephine <josephinekentrhines@gmail.com>
2025-09-25 21:40:43 +0300

    Refactor signup serializer for confirm_password

--- obeeomaapp/serializers.py
+++ obeeomaapp/serializers.py
@@ -4,17 +4,23 @@ from django.contrib.auth.password_validation import validate_password
 
 User = get_user_model()
 
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+
+User = get_user_model()
+
 class SignupSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, validators=[validate_password])
-    password2 = serializers.CharField(write_only=True)
+    confirm_password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password', 'password2')
+        fields = ('username', 'email', 'password', 'confirm_password')
 
     def validate(self, attrs):
-        if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError({"password": "Passwords don’t match."})
+        if attrs['password'] != attrs['confirm_password']:
+            raise serializers.ValidationError({"confirm_password": "Passwords don’t match."})
         return attrs
 
     def create(self, validated_data):
@@ -25,6 +31,7 @@ class SignupSerializer(serializers.ModelSerializer):
         user.set_password(validated_data['password'])
         user.save()
         return user
+
 
 
 class LoginSerializer(serializers.Serializer):
