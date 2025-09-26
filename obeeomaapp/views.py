76090848532f3164e62900dd68bb516e932b95be
Lucy-dev1999 <ainembabaziluciarachel02@gmail.com>
2025-09-26 13:17:01 +0300

    Refactor imports and settings in various files; add pytest configuration

--- obeeomaapp/views.py
+++ obeeomaapp/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.contrib.auth import authenticate, get_user_model
 from rest_framework_simplejwt.tokens import RefreshToken
-from .serializers import (
+from obeeomaapp.serializers import (
     SignupSerializer,
     LoginSerializer,
     PasswordResetSerializer,
