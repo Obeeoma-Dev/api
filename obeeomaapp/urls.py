267d4bda52c4b3ffca373704357d97943a2046bf
namugarurasyda <2100813912@std.kyu.ac.ug>
2025-09-29 06:35:42 +0300

    changed the urls.py

--- obeeomaapp/urls.py
+++ obeeomaapp/urls.py
@@ -22,15 +22,15 @@ schema_view = get_schema_view(
 urlpatterns = [
     # --- Admin Dashboard ---
     path("overview/", OverviewView.as_view(), name="overview"),
-    path("trends/", TrendsView.as_view(), name="trends"),
-    path("engagements/", ClientEngagementView.as_view(), name="engagements"),
-    path("features/", FeaturesUsageView.as_view(), name="features"),
-    path("billing/", BillingView.as_view(), name="billing"),
-    path("invites/", InviteView.as_view(), name="admin-invites"),
-    path("users/", UsersView.as_view(), name="users"),
-    path("users/<int:user_id>/", UserDetailView.as_view(), name="user-detail"),
-    path("reports/", ReportsView.as_view(), name="reports"),
-    path("crisis-insights/", CrisisInsightsView.as_view(), name="crisis-insights"),
+    # path("trends/", TrendsView.as_view(), name="trends"),
+    # path("engagements/", ClientEngagementView.as_view(), name="engagements"),
+    # path("features/", FeaturesUsageView.as_view(), name="features"),
+    # path("billing/", BillingView.as_view(), name="billing"),
+    # path("invites/", InviteView.as_view(), name="admin-invites"),
+    # path("users/", UsersView.as_view(), name="users"),
+    # path("users/<int:user_id>/", UserDetailView.as_view(), name="user-detail"),
+    # path("reports/", ReportsView.as_view(), name="reports"),
+    # path("crisis-insights/", CrisisInsightsView.as_view(), name="crisis-insights"),
 
 
     path("", home, name="home"),   # root URL
@@ -38,7 +38,6 @@ urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
     path("password-reset/", PasswordResetView.as_view(), name="password_reset"),
     path("password-change/", PasswordChangeView.as_view(), name="password_change"),
-
     # --- Swagger Docs ---
     path("swagger/", schema_view.with_ui('swagger', cache_timeout=0), name="schema-swagger-ui"),
 ]
