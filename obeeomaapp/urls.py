c02c71844dcbedc53532a8fcf9193ba698d1413b
Mukhaye Belinda <namuzbelinda@gmail.com>
2025-09-29 22:33:13 +0300

    all  changes

--- obeeomaapp/urls.py
+++ obeeomaapp/urls.py
@@ -22,29 +22,23 @@ schema_view = get_schema_view(
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
-   openapi.Info(
-      title="Obeeoma API",
-      default_version='v1',
-      description="Endpoints for signup, login, reset-password ,change-password",
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
 )
+   
 
 # --- URL patterns ---
 # Now reference it inside urlpatterns
 urlpatterns = [
     # Dashboard / Admin URLs
     path("overview/", OverviewView.as_view(), name="overview"),
-    # path("trends/", TrendsView.as_view(), name="trends"),
-    # path("engagements/", ClientEngagementView.as_view(), name="engagements"),
-    # path("features/", FeaturesUsageView.as_view(), name="features"),
-    # path("billing/", BillingView.as_view(), name="billing"),
-    # path("invites/", InviteView.as_view(), name="admin-invites"),
-    # path("users/", UsersView.as_view(), name="users"),
-    # path("users/<int:user_id>/", UserDetailView.as_view(), name="user-detail"),
-    # path("reports/", ReportsView.as_view(), name="reports"),
-    # path("crisis-insights/", CrisisInsightsView.as_view(), name="crisis-insights"),
+    path("trends/", TrendsView.as_view(), name="trends"),
+    path("engagements/", ClientEngagementView.as_view(), name="engagements"),
+    path("features/", FeaturesUsageView.as_view(), name="features"),
+    path("billing/", BillingView.as_view(), name="billing"),
+    path("invites/", InviteView.as_view(), name="admin-invites"),
+    path("users/", UsersView.as_view(), name="users"),
+    path("users/<int:user_id>/", UserDetailView.as_view(), name="user-detail"),
+    path("reports/", ReportsView.as_view(), name="reports"),
+    path("crisis-insights/", CrisisInsightsView.as_view(), name="crisis-insights"),
 
     # --- Authentication & Home ---
     path("", home, name="home"),
