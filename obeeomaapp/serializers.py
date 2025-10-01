2dfb8e6e59172d3926d232071e840d064ac30be8
Mukhaye Belinda <namuzbelinda@gmail.com>
2025-10-01 09:11:13 +0300

    all merged changes

--- obeeomaapp/serializers.py
+++ obeeomaapp/serializers.py
@@ -3,14 +3,7 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
-from obeeomaapp.models import (
-    User, Organization, Client, RecentActivity,
-    HotlineActivity, ClientEngagement,
-    AIManagement, Subscription,
-    SelfAssessment, MoodCheckIn,
-    SelfHelpResource, ChatbotInteraction,
-    UserBadge, EngagementStreak
-)
+from obeeomaapp.models import *
 
 User = get_user_model()
 
