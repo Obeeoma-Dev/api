e71510ce82ca229972249136513895771907419a
Mukhaye Belinda <namuzbelinda@gmail.com>
2025-10-03 10:10:34 +0300

    Refactor database settings to use explicit

--- obeeomaapp/models.py
+++ obeeomaapp/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
-
+from django.conf import settings
 # --- User & Authentication ---
 class User(AbstractUser):
     ROLE_CHOICES = (
@@ -253,7 +253,7 @@ from django.contrib.auth.models import User
 
 # --- Core Employee Profile ---
 class EmployeeProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
     organization = models.CharField(max_length=100)
     role = models.CharField(max_length=50)
