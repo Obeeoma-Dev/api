941c9c850184b9d9962425b3fa28c23e2943a9f7
kulabako josephine <josephinekentrhines@gmail.com>
2025-10-13 20:38:32 +0300

    frontend render link

--- api/settings.py
+++ api/settings.py
@@ -120,7 +120,8 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",     
+    "http://localhost:5173", 
+    "http://obeeoma.onrender.com",    
 ]
 CORS_ALLOW_ALL_ORIGINS = True
 
