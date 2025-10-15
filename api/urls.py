2953b445352ffca72abeb9dff8be41404e8ab79b
kulabako josephine <josephinekentrhines@gmail.com>
2025-10-15 08:27:15 +0300

    removed the duplicate endpoints from  api/urls.py

--- api/urls.py
+++ api/urls.py
@@ -17,9 +17,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),                    
     path("", include("obeeomaapp.urls")),              
     path("api/v1/", include("obeeomaapp.urls", namespace="v1")),       
-    path("auth/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("auth/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
 ]
