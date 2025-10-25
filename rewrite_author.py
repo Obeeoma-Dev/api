b2c100f706bfc7cd5d9e56b641eb8b357036d1ed
Lucy-dev1999 <ainembabaziluciarachel02@gmail.com>
2025-10-25 17:43:12 +0300

    refactor: Remove commit_callback function for author and committer information

--- rewrite_author.py
+++
@@ -1,7 +0,0 @@
-def commit_callback(commit):
-    if commit.author_email.decode("utf-8") == "ainembabaziluciarachel02@gmail.com":
-        commit.author_name = "Ainembabazi Lucia Rachel".encode("utf-8")
-        commit.author_email = "ainembabaziluciarachel02@gmail.com".encode("utf-8")
-    if commit.committer_email.decode("utf-8") == "ainembabaziluciarachel02@gmail.com":
-        commit.committer_name = "Ainembabazi Lucia Rachel".encode("utf-8")
-        commit.committer_email = "ainembabaziluciarachel02@gmail.com".encode("utf-8")
