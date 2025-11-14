74f5be684498439a58541362612abb3713181976
kulabako josephine <josephinekentrhines@gmail.com>
2025-11-14 12:40:23 +0300

    Un commented out sana api

--- obeeomaapp/urls.py
+++ obeeomaapp/urls.py
@@ -198,5 +198,5 @@ urlpatterns = [
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 
     # Sana_ai.
-    # path("api/", include("sana_ai.urls")),
+    path("api/", include("sana_ai.urls")),
 ]
